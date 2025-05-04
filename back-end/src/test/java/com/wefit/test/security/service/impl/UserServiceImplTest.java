6dd22974959a7f5d73bd38079a27af0e1c846730
fernandolilo <fernando.sistenpro@outlook.com>
2025-05-04 11:35:25 -0300

    UserServiceImplTest ok

---
+++ back-end/src/test/java/com/wefit/test/security/service/impl/UserServiceImplTest.java
@@ -0,0 +1,77 @@
+package com.wefit.test.security.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.wefit.test.entity.Client;
+import com.wefit.test.entity.enums.Role;
+import com.wefit.test.reposiotries.ClientRepository;
+import com.wefit.test.sercurity.service.UserSecurityDetails;
+import com.wefit.test.sercurity.service.impl.UserDetailsServiceImpl;
+import com.wefit.test.service.exeptions.AuthorizationException;
+
+@ExtendWith(MockitoExtension.class) // Usando Mockito com JUnit 5
+public class UserServiceImplTest {
+
+	@InjectMocks
+	private UserDetailsServiceImpl userDetailsService;
+
+	@Mock
+	private ClientRepository clientRepository;
+
+	private Client client;
+
+	@BeforeEach
+	public void setUp() {
+		// Configuração inicial para os testes
+		client = new Client();
+		client.setId(UUID.randomUUID()); // Certifique-se de atribuir um ID único
+		client.setEmail("test@example.com");
+		client.setSenha("password123");
+		client.setRoles(new HashSet<>());
+		client.getRoles().add(Role.USER);
+	}
+
+	@Test
+	public void testLoadUserByUsername_UserFound() {
+		// Configurando o comportamento do mock para retornar o client quando o email é
+		// passado
+		when(clientRepository.findByEmail("test@example.com")).thenReturn(Optional.of(client));
+
+		// Chamando o método que queremos testar
+		UserSecurityDetails userDetails = (UserSecurityDetails) userDetailsService
+				.loadUserByUsername("test@example.com");
+
+		// Verificando os resultados
+		assertNotNull(userDetails);
+		assertEquals(client.getId(), userDetails.getId()); // Comparando o ID correto
+		assertEquals("test@example.com", userDetails.getUsername());
+		assertEquals("password123", userDetails.getPassword());
+	}
+
+	 @Test
+	    public void testLoadUserByUsername_UserNotFound() {
+	        // Configurando o mock para retornar um Optional vazio quando o email não for encontrado
+	        when(clientRepository.findByEmail("test@example.com")).thenReturn(Optional.empty());
+
+	        // Chamando o método que deve lançar a exceção
+	        assertThrows(AuthorizationException.class, () -> {
+	            userDetailsService.loadUserByUsername("test@example.com");
+	        });
+	    }
+	
+	
+}
---
+++ back-end/src/test/java/com/wefit/test/serviceTest/UserServiceTest.java
@@ -0,0 +1,7 @@
+package com.wefit.test.serviceTest;
+
+import com.wefit.test.security.service.UserSecurityDetailsTest;
+
+public interface UserServiceTest {
+	UserSecurityDetailsTest authenticated();
+}
