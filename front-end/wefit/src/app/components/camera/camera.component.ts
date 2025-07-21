d6fb7e22d56754adc01693769b624a5b50b758c3
fernandolilo <fernando.sistenpro@outlook.com>
2025-07-21 15:40:46 -0300

    ajuste em componente de camera

--- front-end/wefit/src/app/components/camera/camera.component.ts
+++ front-end/wefit/src/app/components/camera/camera.component.ts
@@ -1,25 +1,31 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
   styleUrl: './camera.component.scss'
 })
-export class CameraComponent implements OnInit {
+export class CameraComponent implements OnInit, OnDestroy {
   @ViewChild('video') videoRef!: ElementRef<HTMLVideoElement>;
   @ViewChild('canvas') canvasRef!: ElementRef<HTMLCanvasElement>;
 
   @Output() selfieCaptured = new EventEmitter<string>();
 
   capturedImage: string | null = null;
+  private stream?: MediaStream;
 
   ngOnInit(): void {
     this.startCamera();
   }
 
+  ngOnDestroy(): void {
+    this.stopCamera();
+  }
+
   startCamera(): void {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(stream => {
+        this.stream = stream;
         const videoElement = this.videoRef.nativeElement;
         videoElement.srcObject = stream;
         videoElement.play();
@@ -28,6 +34,13 @@ export class CameraComponent implements OnInit {
         console.error('Erro ao acessar a câmera: ', err);
         alert('Não foi possível acessar a câmera.');
       });
+  }
+
+  stopCamera(): void {
+    if (this.stream) {
+      this.stream.getTracks().forEach(track => track.stop());
+      this.stream = undefined;
+    }
   }
 
   capture(): void {
@@ -41,9 +54,18 @@ export class CameraComponent implements OnInit {
     if (context) {
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
       this.capturedImage = canvas.toDataURL('image/jpeg');
-
-      // Emite o evento para o componente pai
-      this.selfieCaptured.emit(this.capturedImage);
     }
+  }
+
+  confirm(): void {
+    if (this.capturedImage) {
+      this.selfieCaptured.emit(this.capturedImage);
+      this.stopCamera();
+    }
+  }
+
+  retake(): void {
+    this.capturedImage = null;
+    this.startCamera();
   }
 }
