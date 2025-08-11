2b42134d3ffd0e9d287bd127e2b876ce847e5428
fernandolilo <fernando.sistenpro@outlook.com>
2025-08-11 20:06:25 -0300

    Iniciado estrutura para micros-servicos de pessoas e autenticacao

---
+++ back-end/client/src/main/java/com/syp/client/enums/TipoPessoa.java
@@ -0,0 +1,6 @@
+package com.syp.client.enums;
+
+public enum TipoPessoa {
+	PESSOA_JURIDICA, // Pessoa com CNPJ (empresa)
+    PESSOA_FISICA    // Pessoa com CPF (individual)
+}
---
+++ back-end/conta/src/main/java/com/invest/entity/enums/TipoInvestimento.java
@@ -0,0 +1,30 @@
+package com.invest.entity.enums;
+
+public enum TipoInvestimento {
+
+    CDB("Renda Fixa", "Baixo"),
+    LCI("Renda Fixa", "Baixo"),
+    LCA("Renda Fixa", "Baixo"),
+    DEBENTURE("Renda Fixa", "Médio"),
+    TESOURO_DIRETO("Renda Fixa", "Baixo"),
+    ACOES("Renda Variável", "Alto"),
+    ETF("Renda Variável", "Alto"),
+    FUNDOS_IMOBILIARIOS("Renda Variável", "Médio"),
+    CRIPTOMOEDA("Alternativo", "Alto");
+
+    private final String categoria;
+    private final String risco;
+
+    TipoInvestimento(String categoria, String risco) {
+        this.categoria = categoria;
+        this.risco = risco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getRisco() {
+        return risco;
+    }
+}
